3f96748a08de32ed34549f3d0690b57e79bfa84b
kylelovestoad <kylelovestoad+github@protonmail.com>
2025-04-06 00:57:37 -0400

    add display prints

--- scanner/scanner.ino
+++ scanner/scanner.ino
@@ -1,9 +1,18 @@
+#include <string.h>
 #include <MFRC522.h>
 #include <SPI.h>
 #include <EEPROM.h>
 #include <Keypad.h>
+#include <LiquidCrystal.h>
 
-#define SET_INITIAL_PASS
+#define LCD_RS 8
+#define LCD_ENABLE 9
+#define LCD_D4 10
+#define LCD_D5 11
+#define LCD_D6 12
+#define LCD_D7 13
+
+// #define SET_INITIAL_PASS
 #define SS_PIN 53
 #define RST_PIN 21
 
@@ -12,6 +21,43 @@
 
 #define PASS_LEN 4
 #define UID_LEN 4
+
+#define LOCK_CHAR 0
+#define UNLOCK_CHAR 1
+#define LEFT_ARROW_CHAR 2
+
+const byte lock[8] = {
+  B01110,
+  B10001,
+  B10001,
+  B11111,
+  B11011,
+  B11011,
+  B11111,
+  B00000
+};
+
+const byte unlock[8] = {
+  B01110,
+  B10001,
+  B10000,
+  B11111,
+  B11011,
+  B11011,
+  B11111,
+  B00000
+};
+
+const byte leftArrow[8] = {
+  B00001,
+  B00011,
+  B00111,
+  B01111,
+  B00111,
+  B00011,
+  B00001,
+  B00000
+};
 
 byte primes[] = { 2, 3, 5, 7 };
 
@@ -29,6 +75,7 @@ byte colPins[COLS] = { 23, 25, 27, 29 };  //connect to the column pinouts of the
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
+LiquidCrystal lcd(LCD_RS, LCD_ENABLE, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 MFRC522 rfid(SS_PIN, RST_PIN);
 
 byte admin_uid[UID_LEN] = {0xD9, 0xAE, 0xD6, 0x00};
@@ -48,11 +95,20 @@ void setup() {
   SPI.begin();
   rfid.PCD_Init();
 
+  lcd.begin(16, 2);
+  lcd.noBlink();
+  lcd.createChar(LOCK_CHAR, lock);
+  lcd.createChar(UNLOCK_CHAR, unlock);
+  lcd.createChar(LEFT_ARROW_CHAR, leftArrow);
+  lcd.clear();
+
 #ifdef SET_INITIAL_PASS
+    lcd_clear_print("Set Initial");
+    lcd.setCursor(0, 1);
+    lcd.print("Password");
     int hash = enter_password();
     for (int i=0; i<2; i++)
       EEPROM.write(i, hash >> (i << 3));
-    Serial.println("Initial Password set");
 #endif
 }
 
@@ -74,6 +130,7 @@ int enter_password() {
     byte key = keypad.getKey();
     while (key < '0' || key > '9')
       key = keypad.getKey();
+    lcd.print("*");
     pass[i] = key - '0';
     hash += pass[i] * pass[i] * primes[i];
   }
@@ -100,6 +157,13 @@ void set_password() {
 }
 
 void login_mode() {
+
+  if (state == ADMIN_LOGGED_IN) {
+    lcd_clear_print("Welcome, Admin");
+  } else {
+    lcd_clear_print("Welcome, User");
+  }
+
   char key = '\0';
 
   while (key != LOGOUT) {
@@ -113,10 +177,20 @@ void login_mode() {
   }
 }
 
+void lcd_clear_print(char* printed) {
+  lcd.clear();
+  lcd.print(printed);
+}
+
 void loop() {
+
   state = LOGGED_OUT;
   char key = '\0';
   bool scanned = false;
+
+  lcd_clear_print("Enter Password: ");
+  lcd.setCursor(6, 1);
+
   while (!scanned && key != '*') {
     key = keypad.getKey();
     scanned = rfid.PICC_IsNewCardPresent();
@@ -132,13 +206,19 @@ void loop() {
   }
 
   if (state != LOGGED_OUT) {
-    Serial.println("Dk won");
+    lcd.clear();
+    lcd.setCursor(1, 0);
+    lcd.print("Access Granted");
+    lcd.setCursor(7, 1);
+    lcd.write(byte(UNLOCK_CHAR));
+    delay(1500);
     login_mode();
+    lcd_clear_print("Goodbye");
   } else {
-    Serial.println("You're mid");
+    lcd_clear_print("Access Denied");
+    lcd.setCursor(7, 1);
+    lcd.write(byte(LOCK_CHAR));
   }
-  Serial.print("state: ");
-  Serial.println(state);
 
   delay(2000);
 }
